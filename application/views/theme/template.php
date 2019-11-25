e06b9672b5c83aed296068eca8ad7d96e9c5e61f
FarhanHanz <farhan030619999@gmail.com>
2019-11-25 22:18:43 +0700

    (edit template)

--- application/views/theme/template.php
+++ application/views/theme/template.php
@@ -11,17 +11,17 @@
 				<td colspan="2" width="1000" height="60" bgcolor="#7FFFD4" align="center"><h1>Menu</h1></td>
 			</tr>
 			<tr>
-				<td width="200" height="15" bgcolor="#778899"><a href="http://localhost/Pendaftaran/index.php/agama">Agama</td>
+				<td width="200" height="15" bgcolor="#778899"><a href="http://localhost/Pendaftaran/index.php/agama" style="color: white">Agama</td>
 				<td rowspan="4" bgcolor="#DCDCDC">Content</td>
 			</tr>
 			<tr>
-				<td width="200" height="15" bgcolor="#778899"><a href="http://localhost/Pendaftaran/index.php/jurusan"> Jurusan</td>
+				<td width="200" height="15" bgcolor="#778899"><a href="http://localhost/Pendaftaran/index.php/jurusan" style="color: white"> Jurusan</td>
 			</tr>
 			<tr>
-				<td width="200" height="15" bgcolor="#778899"><a href="http://localhost/Pendaftaran/index.php/dosen"> Dosen</td>
+				<td width="200" height="15" bgcolor="#778899"><a href="http://localhost/Pendaftaran/index.php/dosen" style="color: white"> Dosen</td>
 			</tr>
 			<tr>
-				<td width="200" height="15" bgcolor="#778899"><a href="http://localhost/Pendaftaran/index.php/calon_mahasiswa"> Calon Mahasiswa</td>
+				<td width="200" height="15" bgcolor="#778899"><a href="http://localhost/Pendaftaran/index.php/calon_mahasiswa" style="color: white"> Calon Mahasiswa</td>
 			</tr>
 			<tr>
 			<td colspan="2" width="1000" height="25" bgcolor="#7FFFD4" align="center">©Copyright<a href="h4nzit.wordpress.com/"> h4nzit.wordpress.com</a></td>
